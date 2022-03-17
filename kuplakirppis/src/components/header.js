b609f19136320d46a04f0943b81c18c9c0054286
paulihaarnio <pauli.haarnio@gmail.com>
2022-03-17 13:34:46 +0200

    header

---
+++ kuplakirppis/src/components/header.js
@@ -0,0 +1,10 @@
+import App from "../App"
+export default function Header(){
+    return
+<header>
+      <h1>
+        kupla kirppis
+      </h1>
+      <h3>tervetuloa shoppailemaan</h3>
+      </header> 
+      }
