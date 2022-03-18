99cc912c30b507dbd562dcd8acb6815352ca8791
paulihaarnio <pauli.haarnio@gmail.com>
2022-03-18 11:30:30 +0200

    carousel

--- kuplakirppis/src/App.js
+++ kuplakirppis/src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer'
+import Carousel from './components/Carousel';
 function App() {
   return (
     <div>
      <Header/>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sit nulla sequi distinctio, iusto delectus architecto dolor cupiditate sint aliquam, culpa expedita. Dolorum mollitia obcaecati unde tempora aliquam possimus labore!</p>
+      <Carousel/>
       <Footer/>
     </div>
     
---
+++ kuplakirppis/src/components/Carousel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+export default function Carousel(){
+    return(
+    <div class="carousel-item">
+  <img src="./design/etusivudog.jpg" alt="..."/>
+  <div class="carousel-caption d-none d-md-block">
+    <h5>Etusivu</h5>
+    <p>tässä on etusivu</p>
+  </div>
+</div>
+    )
+      }
