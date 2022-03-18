26ff07d66c9cfc1de11ccf7c6fc0560b3bf0482f
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-03-18 11:31:27 +0200

    nav ei oo hyvä vielä

--- kuplakirppis/src/components/Nav.js
+++ kuplakirppis/src/components/Nav.js
@@ -1,6 +1,26 @@
 import React from 'react'
 export default function Nav(){
     return(
-       
+        
+        <nav className ="col-md-12 d-none d-md-block sidebar navbar bg-dark">
+        <div className='sidebar-sticky'></div>
+        <ul className ="nav navbar-nav">
+        <li className ="nav-item">
+        <a className ="nav-link" href="#"> Home </a>
+        </li>
+        <li className ="nav-item">
+        <a className ="nav-link" href="#"> Services </a>
+        </li>
+        <li className ="nav-item">
+        <a className ="nav-link" href="#"> Contact </a>
+        </li>
+        <li className ="nav-item">
+        <a className ="nav-link" href="#"> Blogs </a>
+        </li>
+        </ul>
+        </nav>
+        
+        
+        
     )
 }
