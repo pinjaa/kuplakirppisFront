20d366cf38efd0a4843fb06722f790a7e6f13176
paulihaarnio <pauli.haarnio@gmail.com>
2022-03-18 13:42:36 +0200

    carousel

--- kuplakirppis/src/App.js
+++ kuplakirppis/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import BootstrapCarousel from './components/BootstrapCarousel';
+
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
      <Header/>
      <Navbar /> 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sit nulla sequi distinctio, iusto delectus architecto dolor cupiditate sint aliquam, culpa expedita. Dolorum mollitia obcaecati unde tempora aliquam possimus labore!</p>
-      
+        <BootstrapCarousel/>
       <Footer/>
     </div>
     
