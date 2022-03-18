c8dd21d9b86cb640cb77c435dd2d203f7d46a1f8
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-03-18 11:17:49 +0200

    nav

--- kuplakirppis/src/App.js
+++ kuplakirppis/src/App.js
@@ -2,11 +2,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import Nav from './components/Nav';
 function App() {
   return (
     <div>
+      
      <Header/>
+     <Nav />
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sit nulla sequi distinctio, iusto delectus architecto dolor cupiditate sint aliquam, culpa expedita. Dolorum mollitia obcaecati unde tempora aliquam possimus labore!</p>
       <Footer/>
     </div>
