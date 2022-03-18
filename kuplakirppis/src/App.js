d4fbe1e7b83dc2ad60a8ef24ef8ee5038a2fd456
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-03-18 13:14:20 +0200

    navbarin korjausta

--- kuplakirppis/src/App.js
+++ kuplakirppis/src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
      <Header/>
-     <Nav /> 
+     <Navbar /> 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sit nulla sequi distinctio, iusto delectus architecto dolor cupiditate sint aliquam, culpa expedita. Dolorum mollitia obcaecati unde tempora aliquam possimus labore!</p>
       
       <Footer/>
