22ba73166bab1905c603b689fb1a283ebad8f79a
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-03-22 11:04:42 +0200

    tuoteryhmä menua aloiteltu

--- kuplakirppis/src/App.js
+++ kuplakirppis/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import BootstrapCarousel from './components/BootstrapCarousel';
+import Categories from './components/Categories';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <div>
      <Header/>
      <Navbar /> 
+     <Categories />
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sit nulla sequi distinctio, iusto delectus architecto dolor cupiditate sint aliquam, culpa expedita. Dolorum mollitia obcaecati unde tempora aliquam possimus labore!</p>
         <BootstrapCarousel/>
       <Footer/>
---
+++ kuplakirppis/src/components/Categories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Categories() {
+  return (
+    <div id='categories'>
+        <ul>
+            <a href="#"><li>Vaatteet</li></a>
+            <a href="#"><li>Elektroniikka ja kodinkoneet</li></a>
+            <a href="#"><li>Vapaa-aika</li></a>
+            <a href="#"><li>Keittiö</li></a>
+            <a href="#"><li>Hifi</li></a>
+            <a href="#"><li>Koti ja asuminen</li></a>
+            <a href="#"><li>Ajoneuvot</li></a>
+        </ul>
+    </div>
+  )
+}
+
