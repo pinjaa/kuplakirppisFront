0da39563e5ea6459c6ea3f38c549f53c8313c83c
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-03-23 14:42:39 +0200

    router asennettu ja säädetty

--- kuplakirppis/src/App.js
+++ kuplakirppis/src/App.js
@@ -1,29 +1,16 @@
 
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import BootstrapCarousel from './components/BootstrapCarousel';
-import Categories from './components/Categories';
+import {Routes, Route} from 'react-router-dom';
+import Frontpage from './components/Frontpage';
+
 
 
 function App() {
   return (
-    <div className='container-fluid'>
-      <div className='row g-0'>
-        <div className='category-div col-2'>
-          <Categories />
-        </div>
-        <div className='col'>
-          <Navbar />  
-        
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sit nulla sequi distinctio, iusto delectus architecto dolor cupiditate sint aliquam, culpa expedita. Dolorum mollitia obcaecati unde tempora aliquam possimus labore!</p>
-          <BootstrapCarousel/>
-        </div>
-        <Footer/>
-      </div>
-    </div>
+    <Routes>
+      <Route path='/' element={<Frontpage />} />
+    </Routes>
     
   );
 }
