aa67a65d08175bcccf7dd2aba38a71d31c4daa19
booennm <booennm@gmail.com>
2022-03-24 13:46:20 +0200

    shopping cart dropdown

--- kuplakirppis/src/components/Navbar.js
+++ kuplakirppis/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Search } from 'react-bootstrap-icons';
+import { Cart3 } from 'react-bootstrap-icons';
 
 export default function Navbar(){
     return (
@@ -25,14 +26,24 @@ export default function Navbar(){
             <a class="nav-link" href="#">KIRJAUDU</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              OSTOSKORI
+            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <Cart3 size={30}/>
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+              <div class="dropdown-item cart-item" href="#">
+                <img src='https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074__480.jpg'></img>
+                <span class="cart-name">Hyvä koira</span>
+                <span class='cart-price'>50€</span>
+              </div>
+              <div class="dropdown-item cart-item" href="#">
+                <img src='https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074__480.jpg'></img>
+                <span class="cart-name">Hyvä koira</span>
+                <span class='cart-price'>50€</span>
+              </div>
               <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
+              <p class="dropdown-item">Yhteensä:<span class="cart-price">2€</span></p>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Siirry kassalle</a>
             </div>
           </li>
           
