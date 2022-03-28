8486a1d8425f9c73a47744c5739b307e43d77a59
Lepias <65459267+Lepias@users.noreply.github.com>
2022-03-28 10:00:47 +0300

    Karuselli esityskunnossa

--- kuplakirppis/src/components/BootstrapCarousel.js
+++ kuplakirppis/src/components/BootstrapCarousel.js
@@ -8,41 +8,41 @@ export default function BootstrapCarousel() {
   <Carousel.Item>
     <img
       className="d-block"
-      style={{marginLeft: 400,height:400}}
-      src="https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074__480.jpg"
+      style={{marginLeft: 275,height:400}}
+      src={kuplalogo}
       alt="koira"
     />
     <Carousel.Caption>
-      <h3>Kuplakirppis</h3>
-      <p>Tutustu laajaan tuotevalikoimaamme</p>
+      <h3>Tervetuloa myymäläämme!</h3>
+      <p>Kirppiskatu 3</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block"
-    style={{marginLeft: 400,height:400}}
+    style={{marginLeft: 275,height:400}}
       src={kuplalogo}
       alt="Second slide"
     />
 
     <Carousel.Caption>
-      <h3>Myy tuotteitasi verkkosivuillamme!</h3>
-      <p>Tulossa pian...</p>
+      <h3>Pöytävaraukset netistä!</h3>
+      <p>Hinnat alkaen 15€ viikko</p>
     </Carousel.Caption>
   </Carousel.Item>
-  <Carousel.Item>
+{/*   <Carousel.Item>
     <img
       className="d-block"
-      style={{marginLeft: 400,height:400}}
+      style={{borderRadius:25, marginLeft: 375,height:400}}
       src="https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074__480.jpg"
       alt="Third slide"
     />
 
     <Carousel.Caption>
-      <h3>Koira</h3>
-      <p>Ostakaa hyvä koira Rajakylästä</p>
+      <h3></h3>
+      <p>Hinnat alkaen 15€ viikko</p>
     </Carousel.Caption>
-  </Carousel.Item>
+  </Carousel.Item> */}
 </Carousel>
     </div>
   )
