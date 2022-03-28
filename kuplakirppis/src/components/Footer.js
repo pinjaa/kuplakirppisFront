4d6bf4018f0f44d2ca91d6b3d0ac4f7cdd89436f
Lepias <65459267+Lepias@users.noreply.github.com>
2022-03-28 14:11:49 +0300

    footeri

--- kuplakirppis/src/components/Footer.js
+++ kuplakirppis/src/components/Footer.js
@@ -2,7 +2,8 @@ import React from 'react'
 export default function Footer(){
     return(
         <footer>
-            <h5>Kuplakirppis - Suomalainen verkkokauppa</h5>
+            <h5
+            style={{padding:5}}>Kuplakirppis - Suomalainen verkkokauppa</h5>
             
         </footer>
     )
