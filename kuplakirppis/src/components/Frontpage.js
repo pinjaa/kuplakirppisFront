67b0466b81330b9d04382da12ba1d335d26fe952
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-03-29 11:06:12 +0300

    url lisätty

--- kuplakirppis/src/components/Frontpage.js
+++ kuplakirppis/src/components/Frontpage.js
@@ -5,12 +5,14 @@ import Navbar from './Navbar';
 import BootstrapCarousel from './BootstrapCarousel';
 import Categories from './Categories';
 
+const URL = 'http://localhost/kuplakirppisBack/';
+
 export default function Frontpage() {
   return (
     <div className='container-fluid'>
       <div className='row g-0'>
         <div className='category-div col-2'>
-          <Categories />
+          <Categories url={URL}/>
         </div>
         <div className='col'>
           <Navbar />  
