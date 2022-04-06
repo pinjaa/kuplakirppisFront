fcaf25134b9d35a7f20a4e54a88e677c4792318d
booennm <booennm@gmail.com>
2022-04-06 10:09:25 +0300

    visuaalista parantelua

--- kuplakirppis/src/components/BootstrapCarousel.js
+++ kuplakirppis/src/components/BootstrapCarousel.js
@@ -3,32 +3,24 @@ import {Carousel} from 'react-bootstrap'
 import kuplalogo from '../images/kuplalogo2.0.png'
 export default function BootstrapCarousel() {
   return (
-    <div>
-        <Carousel variant='dark' style={{alignItems:'center'}}>
+    <div style={{textAlign: 'center'}}>
+      <img src={kuplalogo}
+      style={{
+        width: '30vw',
+        height: '100%',
+        }}></img>
+        <Carousel variant='dark' style={{textAlign:'center'}}>
   <Carousel.Item>
-    <img
-      className="d-block"
-      style={{height:350}}
-      src={kuplalogo}
-      alt="koira"
-    />
-    <Carousel.Caption>
+    <div style={{marginBottom: '3em'}}>
       <h3>Tervetuloa myymäläämme!</h3>
       <p>Kirppiskatu 3</p>
-    </Carousel.Caption>
+    </div>
   </Carousel.Item>
   <Carousel.Item>
-    <img
-      className="d-block"
-    style={{height:350}}
-      src={kuplalogo}
-      alt="Second slide"
-    />
-
-    <Carousel.Caption>
+    <div style={{marginBottom: '3em'}}>
       <h3>Pöytävaraukset netistä!</h3>
       <p>Hinnat alkaen 15€ viikko</p>
-    </Carousel.Caption>
+    </div>
   </Carousel.Item>
 {/*   <Carousel.Item>
     <img
--- kuplakirppis/src/components/Navbar.js
+++ kuplakirppis/src/components/Navbar.js
@@ -7,13 +7,12 @@ import Login from './Login';
 export default function Navbar({cart}){
     return (
       <nav class="navbar navbar-expand-sm navbar-light"
-      style={{backgroundColor: '#b0ffc6'}}>
+      style={{backgroundColor: '#b0ffc6', paddingLeft: '2em', paddingRight: '2em'}}>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="container"
-      style={{marginLeft: 500}}>
+      <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class="form input-group"> <input type="text" class="form-control form-input" placeholder="Etsi löytöjä..."></input><div className='input-group-text'><span><Search color='#8ceda7' id='search-icon'/></span></div></div>
@@ -39,7 +38,7 @@ export default function Navbar({cart}){
      
             </div>
           </li>
-          <li className='nav-item'>
+          <li className='nav-item' style={{marginLeft: '2em'}}>
             <Cart cart={cart} />
           </li>
           
