4bffd28e5d064a08cc0b1ddfb5acae6397952d79
booennm <booennm@gmail.com>
2022-04-06 12:30:46 +0300

    ostoskorin badge

--- kuplakirppis/src/components/Cart.js
+++ kuplakirppis/src/components/Cart.js
@@ -5,8 +5,12 @@ import {Link} from 'react-router-dom'
 export default function Cart({cart}) {
   return (
     <Link to="/order">
-        <Cart3 size={30}/>
-        <span>{cart.length}</span>
+        <Cart3 size={30} class="position-relative"/>
+        <span 
+        class="position-absolute bottom-0 translate-middle badge rounded-pill"
+        style={{backgroundColor: '#A05EF0', color: 'black'}}>
+        {cart.length}
+        </span>
     </Link>
   )
 }
