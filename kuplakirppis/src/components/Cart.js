ea1d7807f99bd8a000f8b1f2ce1c65c767c20f3b
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-11 14:53:17 +0300

    classit muutettu classnameksi

--- kuplakirppis/src/components/Cart.js
+++ kuplakirppis/src/components/Cart.js
@@ -5,10 +5,10 @@ import {Link} from 'react-router-dom'
 export default function Cart({cart}) {
   return (
     <Link to="/order">
-        <Cart3 size={30} class="position-relative"/>
+        <Cart3 size={30} className="position-relative"/>
         <span 
-        class="position-absolute bottom-0 translate-middle badge rounded-pill"
-        style={{backgroundColor: '#A05EF0',}}>
+        className="position-absolute bottom-0 translate-middle badge rounded-pill" 
+        style={{backgroundColor: '#A05EF0'}}>
         {cart.length}
         </span>
     </Link>
