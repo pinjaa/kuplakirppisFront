3179c861d11faeab10c2feb0e85c9cfa23f6f187
rikulis <remesrikua@gmail.com>
2022-04-13 10:33:58 +0300

    NAvbar

--- kuplakirppis/src/components/Navbar.js
+++ kuplakirppis/src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar({cart}){
   function executeSearch(e) {
     if (e.charCode === 13) {
       e.preventDefault();
-      navigate('/search/' + search);
+      Navigate('/products/' + search);
     }
   }
 
