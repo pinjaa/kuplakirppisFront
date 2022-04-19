72c7ed5fe0ae08c1e9d1f832e858e5a2dd2f4a7c
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-19 14:50:49 +0300

    korjattu map

--- kuplakirppis/src/components/CategoryList.js
+++ kuplakirppis/src/components/CategoryList.js
@@ -20,13 +20,12 @@ export default function CategoryList({url, selectedCategory, setSelectedCategory
     }, [selectedCategory])
 
     function onCategoryChange(value) {
-        setSelectedCategory(categories.filter(item => item.id === value));
+        setSelectedCategory(categories.filter(item => item.ktg_nro === value));
     }
-    
   return (
     <select value={selectedCategory?.id} onChange={(e) => onCategoryChange(e.target.value)}>
         {categories.map((category) => (
-            <option key={category.id} value={category.id}>{category.name}</option>
+            <option key={category.ktg_nro} value={category.ktg_nro}>{category.ktg_nimi}</option>
         ))}
     </select>
   )
