1adcc3f8ae2ccc48040b2fe9fab1e9546149c0d7
Lepias <topiasleppakoski@gmail.com>
2022-05-02 16:08:25 +0300

    login ja logout toimii

--- kuplakirppis/src/components/Login.js
+++ kuplakirppis/src/components/Login.js
@@ -1,23 +1,15 @@
 import React from 'react'
 import axios from 'axios';
 import { useState } from 'react';
+import { Display } from 'react-bootstrap-icons';
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [pword, setPword] = useState("");
-   /* useEffect(() => {
-        axios.get(url + 'modules/login.php/')
-        .then((response) => {
-            const json = response.data;
-            console.log(response.data);
-           
-        }).catch(error => {
-            alert(error.response === undefined ? error : error.response.data.error)
-        })
-      }, []) */
       
         function empty(e){
           e.preventDefault();
+          
           if(email == "" || pword == ""){
            // alert("et voi asettaa tyhjiä arvoja")
             document.getElementById("erroralert").innerHTML="<div class='alert alert-success' role='alert' id='erroralert'> Et voi asettaa tyhjiä arvoja! </div>"
@@ -34,19 +26,44 @@ export default function Login() {
               }
             })
             .then((response) => {
-              document.getElementById("erroralert").innerHTML="<div class='alert alert-success' role='alert' id='erroralert'>"+ response.data +"  </div>"
+              document.getElementById("erroralert").innerHTML="<div class='alert alert-success' role='alert' id='erroralert'>"+ response.data +"  </div>";
+              document.getElementById("loginForm").style=" display : none"
+              document.getElementById("logOutForm").style=" display : block"
           }).catch(error => {
               alert(error.response === undefined ? error : error.response.data.error)
           })
           
           }
-          
+         
         } 
         
+      function logout(e){
+        e.preventDefault();
+        
       
+          const json = JSON.stringify({
+            email: email,
+          });
+
+          axios.post("http://localhost/kuplakirppisBack/modules/logout.php", json, {
+            headers: {
+              'Accept' : 'application/json',
+              'Content-Type' : 'application/json'
+            }
+          })
+          .then((response) => {
+            document.getElementById("erroralert").innerHTML="<div class='alert alert-success' role='alert' id='eoralert'>"+ response.data +"  </div>";
+            document.getElementById("loginForm").style=" display : block"
+            document.getElementById("logOut").style=" display : none"
+        })
+        
+        
+       
+      } 
      
   return (
-   <form onSubmit={empty}>
+   <form onSubmit={empty} >
+     <div id='loginForm'>
         <label><h6>Kirjaudu sisään.</h6></label>
 
          <input type="email" name="email" id="login_email" placeholder='sähköpostiosoite' value={email} onChange={e => setEmail(e.target.value)}/> 
@@ -55,7 +72,14 @@ export default function Login() {
         <p></p>
 
         <button type='submit' className='btn btn-success'>Kirjaudu sisään</button>
-        <div id="erroralert"></div>
+  
+        </div>
+    <div id="erroralert"></div>
+        <div id="logOutForm" style={{display : 'none'}}>
+        <form id='logOut'  onSubmit={logout}>
+        <button type='submit' className='btn btn-danger'  value={email} onChange={ e => setEmail(e.target.value)}> Kirjaudu ulos</button>
+        </form>
+        </div>
       </form> 
       
   )
