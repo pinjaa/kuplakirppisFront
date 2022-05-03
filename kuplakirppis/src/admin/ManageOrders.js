e8136916b00c8707c699095761852c8340d08c48
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-05-03 11:33:59 +0300

    luotu adminille tilausten tarkastelu

---
+++ kuplakirppis/src/admin/ManageOrders.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import uuid from 'react-uuid'
+
+export default function ManageOrders({url}) {
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+      axios.get(url + 'products/getorders.php')
+      .then((response) => {
+          const json = response.data;
+          setOrders(json);
+      }).catch(error => {
+          alert(error.response === undefined ? error : error.response.data.error)
+      })
+    }, [])
+
+
+  return (
+    <>
+        <Link to="../admin/Admin" style={{float:"left"}}>Takaisin</Link><br />
+        <h3>Tarkastele tilauksia</h3>
+        <table className='table'>
+            <thead>
+                <tr key={uuid()}>
+                    <th>Tilausnumero</th>
+                    <th>Asiakasnumero</th>
+                    <th>Tilauspäivämäärä</th>
+                </tr>
+            </thead>
+            <tbody>
+                {orders.map((order) => (
+                    <tr key={uuid()}>
+                        <td>{order.tilausnro}</td>
+                        <td>{order.asiakasnro}</td>
+                        <td>{order.tilauspvm}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </>
+  )
+}
