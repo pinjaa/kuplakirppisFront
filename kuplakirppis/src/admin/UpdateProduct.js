45148056722d3dee63a429a497ed4c8a2f112e97
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-05-03 09:23:19 +0300

    korjattu price usestate

--- kuplakirppis/src/admin/UpdateProduct.js
+++ kuplakirppis/src/admin/UpdateProduct.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom';
-import {Container, Row, Col, Card, Button} from 'react-bootstrap'
-import { Cart3 } from 'react-bootstrap-icons';
 
 export default function UpdateProduct({url}) {
     const [products, setProducts] = useState([]);
     const [name, setName] = useState(null)
-    const [price, setPrice] = useState(null)
+    const [price, setPrice] = useState(0)
     const [description, setDescription] = useState(null)
 
     let params = useParams();
@@ -22,19 +20,19 @@ export default function UpdateProduct({url}) {
       })
     }, [params])
 
-    function updateProduct(e) {
+    async function updateProduct(e) {
       e.preventDefault();
       const json = JSON.stringify({name: name, price: price, description: description});
-      axios.post(url + 'products/updateProducts.php/' + params.categoryId + '/' + params.productId, json,{
-          headers: {
+      console.log(json);
+      await axios.post(url + 'products/updateProducts.php/' + params.categoryId + '/' + params.productId, json,{
+          headers: {  
               'Content-Type' : 'application/json'
           }
       })
       .then((response) => {
         const json = response.data
-          setName(json.tuotenimi)
-          setPrice(json.hinta)
-          setDescription(json.kuvaus)
+        console.log(json);
+          document.getElementById("alert").innerHTML="<div class='alert alert-success' role='alert' id='erroralert'>"+ json.alert +"  </div>"
       }).catch(error => {
           alert(error.response === undefined ? error : error.response.data.error);
       });
@@ -52,17 +50,18 @@ export default function UpdateProduct({url}) {
 
             <div className='col' key={product.id} >
                 <Link to="../admin/ManageProducts" style={{float:"left"}}>Takaisin</Link><br />
+                <h3>Muokkaa tuotteen tietoja</h3>
                 <label>Tuotenimi: </label>
-                <input type="text" name='name' defaultValue={product.tuotenimi} onChange={e => setName(e.target.value)}/><br />
+                <input type="text" name='name' placeholder={product.tuotenimi} onChange={e => setName(e.target.value)}/><br />
                 <label>Hinta: </label>
-                <input type="text" name='price' defaultValue={product.hinta} onChange={e => setPrice(e.target.value)}/><br />
+                <input type="text" name='price' placeholder={product.hinta} onChange={e => setPrice(e.target.value)}/><br />
                 <label>Kuvaus: </label>
-                <input type="text" name='description' style={{'width':"30em",'height':"4em"}} defaultValue={product.kuvaus} onChange={e => setDescription(e.target.value)}/><br />
+                <input type="text" name='description' style={{'width':"30em",'height':"4em"}} placeholder={product.kuvaus} onChange={e => setDescription(e.target.value)}/><br />
                 <button>Tallenna muutokset</button>
-                 
             </div>
             
         ))}
+        <div id='alert'></div>
         </form>
           
       </div>
